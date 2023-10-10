052ff9cdf9fc1894d6dc426b694df3d631e7111e
Gino Paoletti Geuna <ginopaolettigeuna30@gmail.com>
2023-10-10 02:33:57 -0300

    Configuracion de JPA + Arquitectura en capas
    Configuracion de JPA y se añadieron todos los paquetes utilizando una arquitectura multi capas

---
+++ src/main/java/com/gino/siscripto/SisCriptoApplication.java
@@ -0,0 +1,13 @@
+package com.gino.siscripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisCriptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisCriptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gino/siscripto/SisCriptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gino.siscripto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisCriptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
