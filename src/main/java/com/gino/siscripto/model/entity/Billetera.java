8dd420e5f5e9035eb9a5e856b0a9128e389bc2c2
Gino Paoletti Geuna <ginopaolettigeuna30@gmail.com>
2023-10-10 13:23:35 -0300

    Estructura Usuario y Billetera con JPA y relacion @OneToMany

--- src/main/java/com/gino/siscripto/model/entity/Billetera.java
+++ src/main/java/com/gino/siscripto/model/entity/Billetera.java
@@ -1,12 +1,13 @@
 package com.gino.siscripto.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -15,7 +16,11 @@ import lombok.NoArgsConstructor;
 @Table(name = "billetera")
 public class Billetera {
     @Id
-    private String id;
-    @ManyToOne
+    @Column(name = "id_billetera")
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) //false: Cada instancia de Billetera debe estar asociada obligatoriamente a una instancia de Usuario
+    @OnDelete(action = OnDeleteAction.CASCADE) //Si elimino un usuario, se eliminan sus billeteras asociadas
     private Usuario usuario;
+    @Column(name = "saldo_billetera")
+    private Float saldo;
 }
